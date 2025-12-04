08b25a8b786fcf2d901f15b139bbcb93517ff188
Fernando Juárez <ferxjuarez@gmail.com>
2025-12-04 11:21:42 +0000

    ese detalle del botón sigue tapando las letras del botón

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -41,14 +41,18 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, children, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        <span className="relative z-10 flex items-center justify-center gap-2">
+          {children}
+        </span>
+      </Comp>
     );
   }
 );
